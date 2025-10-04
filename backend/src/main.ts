bb8f0302e6fb7b9a9cc7c11bfc181fb4e2fad304
Angel David Ramirez Alvarez <angeldavidramirezalvarez@COBOADM-IT-P05.local>
2025-10-04 01:57:38 -0500

    feat: add api prefix for project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,11 @@ import { ResponseInterceptor } from './common/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
