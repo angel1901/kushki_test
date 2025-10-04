ca170c8bbbe8b6a3087d79fcbdb7d16ae8782a64
Angel David Ramirez Alvarez <angeldavidramirezalvarez@COBOADM-IT-P05.local>
2025-10-04 03:01:06 -0500

    feat: integrate Material theme provider and create theme configuration

---
+++ frontend/src/theme.ts
@@ -0,0 +1,45 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#88ABF2',
+        },
+        secondary: {
+            main: '#88B6F2',
+        },
+        background: {
+            default: '#BFBFBF',
+            paper: '#BFBFBF',
+        },
+        text: {
+            primary: '##0D0D0D',
+        },
+        success: {
+            main: '#1cc88a',
+        },
+        warning: {
+            main: '#f6c23e',
+        },
+        error: {
+            main: '#e74a3b',
+        },
+        info: {
+            main: '#36b9cc',
+        },
+        divider: '#dee2e6',
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none'
+                },
+            },
+        },
+    },
+});
+
+export default theme;
